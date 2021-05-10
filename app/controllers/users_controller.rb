bd2edbf0c0fb07571dda4d042d01ee825d641af5
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-10 14:13:05 -0400

    new and create actions added in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show]
+
+            
+        
+    def new
+        @user = User.new
+    end
+
+    def show 
+
+    end
+
+    def create
+
+        @user = User.new(user_params)
+       
+
+        
+        if @user.save
+            session[:user_id] = @user.id
+             
+            redirect_to user_path(@user)    
+        else
+            render :new
+        end
+    end
+            
+
+
+
+    def find_user
+        @user = User.find_by_id(params[:id])
+    end
+
+        def user_params
+            params.require(:user).permit(:name, :username, :email, :password )
+
+        end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    
     has_many :user_languages
     has_many :languages, through: :user_languages
+
+
+    validates :username, presence: true, uniqueness: true 
 end
