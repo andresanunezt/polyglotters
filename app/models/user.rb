5ceeb96593a225c7e292ca0af79839f15bb9aaa3
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-10 13:33:01 -0400

    commit tree 77660c581fd140ac796f7456253def6da75af27e

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :user_languages
+    has_many :languages, through: :user_languages
 end
--- db/migrate/20210510164355_create_user_languages.rb
+++ db/migrate/20210510164355_create_user_languages.rb
@@ -1,6 +1,16 @@
 class CreateUserLanguages < ActiveRecord::Migration[6.1]
   def change
     create_table :user_languages do |t|
+      t.text :why_learn
+      t.text :goals
+      
+      
+      t.boolean :enjoying
+      
+      t.integer :time_spent
+      
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :language, null: false, foreign_key: true
 
       t.timestamps
     end
