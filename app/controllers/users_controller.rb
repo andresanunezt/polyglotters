be28c8bea1327ef35fad9614f34df92fc5435d91
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-16 17:58:30 -0400

    began styling, added css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,35 +20,29 @@ class UsersController < ApplicationController
     end
 
     def create
-
-        @user = User.new(user_params)
-       
-
         
-        if @user.save
-            session[:user_id] = @user.id
+        @user = User.new(user_params)
+            if @user.save
+                session[:user_id] = @user.id
              
-            redirect_to user_path(@user)    
-        else
-            render :new
-        end
+                redirect_to user_path(@user)    
+            else
+                render :new
+            end
     end
 
     def edit
     end
     
     def update
-        
         @user.update(user_params)
-       
-        redirect_to user_path(@user)
-            
+        redirect_to user_path(@user)    
     end 
             
 
 
 
-
+    private
     
     def find_user
         @user = User.find_by_id(params[:id])
