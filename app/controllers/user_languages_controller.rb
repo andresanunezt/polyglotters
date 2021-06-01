768b1fc3004d93023bc9c5f41cef48d85e18f4ff
Andrés Nuñez <andresanunez98@gmail.com>
2021-06-01 10:39:01 -0400

    cleaned up user_language controller

--- app/controllers/user_languages_controller.rb
+++ app/controllers/user_languages_controller.rb
@@ -11,9 +11,6 @@ class UserLanguagesController < ApplicationController
         end
         if params[:q]
              @user_languages = UserLanguage.search(params[:q].capitalize)
-            if @user_languages == UserLanguage.order(:created_at)
-                flash[:error] = "No"
-            end
             
         end
     end
