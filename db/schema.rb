f95dcde03c8d2b4f71659f06b71d4500e839aa8b
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-12 21:04:47 -0400

    updated schema. previously failed to save

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2021_05_13_232537) do
     t.text "learned"
     t.text "learn_next"
     t.text "review"
-    
     t.boolean "enjoying"
     t.integer "time_spent"
     t.integer "user_id", null: false
